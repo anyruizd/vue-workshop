34ae77bea1e3d7f9ace496945499c1a1ef80fb0f
anyruizd <anyruizd@gmail.com>
2019-06-28 23:58:18 -0500

    Add challenge 3

---
+++ challenge-3/src/components/ProgressBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-container>
+    <h3 class="text-center mt-3">Barra de progreso 🚥</h3>
+    <h3 class="text-center mt-2">{{ contador }}%</h3>
+    <div class="text-center mt-3">
+      <button class="btn btn-danger mb-3" @click="lessContador">-</button>
+      <button class="btn btn-primary mr-1 mb-3" @click="plusContador">+</button>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "ProgressBar",
+  data() {
+    return {
+      contador: 0
+    };
+  },
+  methods: {
+    lessContador() {
+      if (this.contador > 0) {
+        return this.contador--;
+      }
+      return this.contador;
+    },
+    plusContador() {
+      if (this.contador < 100) {
+        return this.contador++;
+      }
+      return this.contador;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
